c07f8a413f0a6f3f1ca9e39607491a3cec89a526
reardonp <reardonp@gmail.com>
2023-03-25 01:26:55 -0400

    cleaned up renderHtml.js

--- src/components/renderHtml.js
+++ src/components/renderHtml.js
@@ -1,22 +1,12 @@
 export function renderHtml(prizeNumber, movies, setMovieJson, setLoadMovieDataDiv) {
-  console.log(prizeNumber)
   if (prizeNumber !== null && movies.length > prizeNumber) {
     fetch(`http://localhost:8888/nfo?folderName=${movies[prizeNumber].option}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
+      .then(response => response.ok ? response.json() : Promise.reject(new Error(`HTTP error! status: ${response.status}`)))
+      .then(data => {
         console.log(data.movie.title);
-        
-        return [setMovieJson(data), setLoadMovieDataDiv(true)];
+        setMovieJson(data);
+        setLoadMovieDataDiv(true);
       })
-      .catch((error) => {
-        console.error("Error posting prize:", error);
-      });
-  } else {
-    return null;
+      .catch(error => console.error("Error posting prize:", error));
   }
 }
